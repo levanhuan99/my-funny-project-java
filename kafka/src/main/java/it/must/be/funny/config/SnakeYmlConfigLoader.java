89c988a2fcdea54c7e50aa273e96bcd3863fa369
le huan <le1171489@gmail.com>
2024-09-11 22:01:40 +0700

    - update log4j2.xml

--- kafka/src/main/java/it/must/be/funny/config/SnakeYmlConfigLoader.java
+++ kafka/src/main/java/it/must/be/funny/config/SnakeYmlConfigLoader.java
@@ -1,12 +1,17 @@
 package it.must.be.funny.config;
 
+import it.must.be.funny.KafkaMain;
 import it.must.be.funny.model.ConfigProperties;
 import it.must.be.funny.model.KafkaConfigProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.yaml.snakeyaml.Yaml;
 import java.io.InputStream;
 
 
 public class SnakeYmlConfigLoader {
+
+    private static final Logger logger = LoggerFactory.getLogger(SnakeYmlConfigLoader.class);
 //    private SnakeYml snakeYml;
     private static final String configPath = "config.yml";
     //todo pass config path when init app
@@ -15,7 +20,7 @@ public class SnakeYmlConfigLoader {
         Yaml yaml = new Yaml();
         try (InputStream inputStream = SnakeYmlConfigLoader.class.getClassLoader().getResourceAsStream(configPath)) {
             ConfigProperties config = yaml.loadAs(inputStream, ConfigProperties.class);
-            System.out.println(config.getKafkaConfigProperties().toString());
+            logger.info("config yml "+ config.getKafkaConfigProperties().toString());
             return config;
         } catch (Exception e) {
             e.printStackTrace();
